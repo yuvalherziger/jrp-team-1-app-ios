18b8caf7f3aef52b41dd517db5af2f83191d1a28
Yuval Herziger <yuval@quintly.com>
2017-03-22 22:05:00 +0100

    tidy up js

---
+++ www/js/jrp-team-1-consumtion-app.js
@@ -0,0 +1,10 @@
+// Initialize your app
+var jrpTeam1ConsumptionApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = jrpTeam1ConsumptionApp.addView('.view-main', {
+    dynamicNavbar: false
+});
