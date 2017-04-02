e22efec29772c5a0865e40318e717ea0c65adc35
Yuval Herziger <yuvalherziger@Yuvals-MacBook-Pro.local>
2017-04-02 22:40:20 +0200

    code style

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 Number.prototype.padLeft = function(base, chr) {
-    var  len = (String(base || 10).length - String(this).length)+1;
-    return len > 0? new Array(len).join(chr || '0')+this : this;
+    var len = (String(base || 10).length - String(this).length) + 1;
+    return len > 0 ? new Array(len).join(chr || '0')+ this : this;
 };
 
 document.addEventListener('deviceready', function () {
